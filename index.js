d0ffaff1645d5710964a2581869a4b9f242f3308
JohnB08 <johnb08.kodehode@gmail.com>
2023-09-20 21:22:32 +0200

    phone scaling complete?

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const fsLabel = document.getElementById("fullscreenlabel");
 const menuBtn = document.getElementById("menucheck");
 const menuLabel = document.getElementById("menubtn");
 const bgBtn = document.getElementById("bgbtn");
+const bgHeader = document.getElementById("bgcolorheader");
 const textBtn = document.getElementById("textbtn");
+const textHeader = document.getElementById("textcolorheader");
 const conBgBtn = document.getElementById("conbgbtn");
+const conHeader = document.getElementById("concolorheader");
 const btnBgBtn = document.getElementById("btnbgbtn");
+const btnBgHeader = document.getElementById("btnbgcolorheader");
 const btnTextBtn = document.getElementById("btntextbtn");
+const btnTextHeader = document.getElementById("btntextcolorheader");
 const allRdmBtn = document.getElementById("allrdmbtn");
+const itWorks = document.getElementById("itworks");
 const path1 = document.getElementById("path1");
 const path2 = document.getElementById("path2");
 //Making the button that makes the two parent containers fullscreen.
@@ -30,28 +36,34 @@ fsSwitch.addEventListener("click", (event) => {
 function randColor() {
   let n = (Math.random() * 0xfffff * 1000000).toString(16);
   val = "#" + n.slice(0, 6);
+  itWorks.textContent = "It Works!";
   return val;
 }
 //I make individual functions for each color, because honestly at my current level i don't know any other way.
 function bgColor() {
   randColor();
   root.style.setProperty("--bgcolor", val);
+  bgHeader.textContent = val;
 }
 function textColor() {
   randColor();
   root.style.setProperty("--text-color", val);
+  textHeader.textContent = val;
 }
 function containerColor() {
   randColor();
   root.style.setProperty("--con-bg-color", val);
+  conHeader.textContent = val;
 }
 function buttonColor() {
   randColor();
   root.style.setProperty("--btn-bg-color", val);
+  btnBgHeader.textContent = val;
 }
 function buttonTextColor() {
   randColor();
   root.style.setProperty("--btn-text-color", val);
+  btnTextHeader.textContent = val;
 }
 //This is for the all-random button, it just runs all the functions on one button.
 function AllRandom() {
